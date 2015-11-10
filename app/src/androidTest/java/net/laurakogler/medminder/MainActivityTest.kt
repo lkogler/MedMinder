2b09bb47029a1c510eb94b367801b16dd73ade2e
Laura <lkogler@gmail.com>
2015-11-09 17:29:02 -0800

    Convert working android test to kotlin

---
+++ app/src/androidTest/java/net/laurakogler/medminder/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package net.laurakogler.medminder
+
+import android.app.Activity
+import android.test.ActivityInstrumentationTestCase2
+import android.widget.TextView
+import junit.framework.Assert
+
+class MainActivityTest : ActivityInstrumentationTestCase2<MainActivity>(MainActivity::class.java) {
+    private var mainActivity: Activity? = null
+
+
+    @Throws(Exception::class)
+    override fun setUp() {
+        super.setUp()
+        mainActivity = activity
+    }
+
+    fun test_itDisplaysHelloWorld() {
+        val textView = mainActivity!!.findViewById(R.id.main_text) as TextView
+        val actual = textView.text.toString()
+        Assert.assertEquals("Hello World!", actual)
+    }
+}
